8f00f3d617716f095ba4f2780c19654a11d6c233
nunexd254 <nunexd254@gmail.com>
2024-06-12 19:25:01 -0300

    252 - Tela de autenticação

--- public_html/index.php
+++ public_html/index.php
@@ -2,22 +2,8 @@
 <?php
 
     require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-    require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+    require_once(dirname(__FILE__, 2) . '/src/views/login.php');
     //ini_set('display_errors',1);
-        //Database::getConnection();
- $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-        //echo $user->getSelect();
-        //echo User::getSelect(['id' => 1], 'name, email');
-        echo '<br>';
-        print_r(User::get(['id'=>1],'name','email'));
-        echo '<br>';
-        echo '<br>';
-        echo '<br>';
-        foreach(User::get([],'name') as $user){
-            echo $user->name;
-            echo '<br>';
-        }
-        //echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
-        //var_dump(Database::getConnection());
+    
        
    
