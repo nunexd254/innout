dc6bf40a0b2f8931693f22ad9d4fc77226895fd0
nunexd254 <nunexd254@gmail.com>
2024-09-13 14:48:52 -0300

    Authentication

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
     require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-    require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+    require_once(dirname(__FILE__, 2) . '/src/views/login.php');
 
     // $sql = 'select * from users';
     // $result = Database::getResultFromQuery($sql);
@@ -10,15 +10,15 @@
     //     echo '<br>';
     // }
 
-    $user = new User(['name' => 'Lucas' , 'email' => 'lucas@cod3r.com.br']);
+    //$user = new User(['name' => 'Lucas' , 'email' => 'lucas@cod3r.com.br']);
     //echo $user->getSelect();
     // echo User::getSelect(['id' => 1], 'name, email');
     // echo '<br>';
     // echo User::getSelect(['name' => 'Chaves']);
-    print_r(User::get(['id' => 1], 'name, email'));
-    echo '<br>';
+    // print_r(User::get(['id' => 1], 'name, email'));
+    // echo '<br>';
 
-    foreach(User::get([], 'name') as $user) {
-        echo $user->name;
-        echo '<br>';
-    }
+    // foreach(User::get([], 'name') as $user) {
+    //     echo $user->name;
+    //     echo '<br>';
+    // }
