5d930ac4733052d465a3cbce73a94c19e83a3182
nunexd254 <nunexd254@gmail.com>
2024-09-13 12:28:34 -0300

    Class User

--- public/index.php
+++ public/index.php
@@ -1,4 +1,16 @@
 <?php
-    require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+    require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+    require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-    Database::getConnection();
+    // $sql = 'select * from users';
+    // $result = Database::getResultFromQuery($sql);
+
+    // while($row = $result->fetch_assoc()) {
+    //     print_r($row);
+    //     echo '<br>';
+    // }
+
+    $user = new User(['name' => 'Lucas' , 'email' => 'lucas@cod3r.com.br']);
+    print_r($user);
+    $user->email = 'lucas_alterado.com.br';
+    print_r($user->email);
