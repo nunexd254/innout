4b0c19de2c1248a2d80375b15897af1dfb5b68e6
nunexd254 <nunexd254@gmail.com>
2024-09-19 18:35:05 -0300

    259 Header

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
     require_once(dirname(__FILE__, 2) . '/src/config/config.php');
     
-    $uri = urldecode($_SERVER['REQUEST_URI']);
+    $uri = urldecode(
+        parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    );
 
-    if($uri === '/' || $uri === '') {
+    if($uri === '/' || $uri === '' || $uri === 'index.php') {
         $uri = '/login.php';
     }
 
