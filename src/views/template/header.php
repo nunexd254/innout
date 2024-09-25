039e74a7924f04e0758db133a043f43b866b361d
nunexd254 <nunexd254@gmail.com>
2024-09-25 16:01:07 -0300

    263 Css Template

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -13,7 +13,7 @@
     <body>
         <header class="header">
             <div class="logo">
-                <i class="icofont-travelling mr-2"></i>
+                <i class="icofont-travelling mr-2 "></i>
                 <span class="font-weight-light"> In </span>
                 <span class="font-weight-bold mx-2"> N'</span>
                 <span class="font-weight-light"> Out </span>
@@ -23,6 +23,21 @@
                 <i class="icofont-navigation-menu"></i>
             </div>
             <div class="spacer">
-
+                <div class="dorpdown">
+                    <div class="dropdown-button">
+                        <span>Usuário Mock</span>
+                        <i class="icofont-simple-down ml-2"></i>
+                    </div>
+                    <div class="dropdown-content">
+                        <ul class="nav-list">
+                            <li class="nav-item">
+                                <a href="logout.php">
+                                    <i class="icofont-logout mr-2"></i>
+                                    Sair
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </header>
