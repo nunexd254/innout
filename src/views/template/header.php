30c6abb1dc8c7f36d18193ac255c54d150e4a0fa
nunexd254 <nunexd254@gmail.com>
2024-09-25 16:51:01 -0300

    265 Css Template

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -22,22 +22,24 @@
             <div class="menu-toggle mx-3">
                 <i class="icofont-navigation-menu"></i>
             </div>
-            <div class="spacer">
-                <div class="dorpdown">
+            <div class="spacer"></div>
+                <div class="dropdown">
                     <div class="dropdown-button">
-                        <span>Usuário Mock</span>
-                        <i class="icofont-simple-down ml-2"></i>
+                        <span class="ml-3">Usuário Mock</span>
+                        <i class="icofont-simple-down mx-2"></i>
+                        <div class="dropdown-content">
+                            <ul class="nav-list">
+                                <li class="nav-item">
+                                    <a href="logout.php">
+                                        <i class="icofont-logout mr-2"></i>
+                                        Sair
+                                    </a>
+                                </li>
+                                
+                            </ul>
+                        </div>
                     </div>
-                    <div class="dropdown-content">
-                        <ul class="nav-list">
-                            <li class="nav-item">
-                                <a href="logout.php">
-                                    <i class="icofont-logout mr-2"></i>
-                                    Sair
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    
                 </div>
-            </div>
+            
         </header>
