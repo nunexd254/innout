4d7ace66e18783d617edd28e84b123791c307c39
nunexd254 <nunexd254@gmail.com>
2025-01-15 21:06:16 -0300

    252 Layout tela de login

--- public/index.php
+++ public/index.php
@@ -1,32 +1,5 @@
 <?php 
     require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-    require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+    require_once(dirname(__FILE__, 2) . '/src/views/login.php');
 
-    $user = new User(['name' => 'Douglas', 'email' => 'email@gmail.com']);
-    //echo 'Fim';
-    print_r($user);
-    echo '<br><br>';
-    $user->email = 'douglas_alterado@gmail.com';
-     echo '<br><br>';
-    print_r(User::get(['id'=>1], 'name, email'));
-    echo '<br><br>';
-    foreach(User::get([], 'name') as $user) {
-        echo $user->name;
-        echo '<br>';
-    }
-     // echo $user->getSelect();
-    // echo '<br><br>';
-    // echo User::getSelect(['id' => 1],'name,email');
-    // echo '<br><br>';
-    // echo User::getSelect(['name'=>'Chaves']);
-    //print_r($user->email);
-    /*
-    $sql = "select * from users";
-
-    $result = Database::getResultFromQuery($sql);
-
-    while($row = $result->fetch_assoc()){
-        print_r($row);
-        echo '<br>';
-    }
-    */    
+ 
