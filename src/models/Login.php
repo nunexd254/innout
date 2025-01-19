9ae77994149ff22aa087df66da849296444d2115
nunexd254 <nunexd254@gmail.com>
2025-01-19 12:33:18 -0300

    256 Validacoes Login

--- src/models/Login.php
+++ src/models/Login.php
@@ -3,7 +3,22 @@
 
     class Login extends Model {
 
+        public function validate() {
+            $errors = [];
+            if(!$this->email) {
+                $errors['email'] = 'Informe o e-mail';
+            }
+            if(!$this->password) {
+                $errors['password'] = 'Informe a senha';
+            }
+
+            if(count($errors) > 0) {
+                throw new ValidationException($errors);
+            }
+        }
+
         public function checkLogin() {
+            $this->validate();
             $user = User::getOne(['email' => $this->email]);
             if($user) {
                 if($user->end_date) {
