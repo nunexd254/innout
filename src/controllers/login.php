8382fa139ce1350803ac607b28f21fd68d262933
nunexd254 <nunexd254@gmail.com>
2025-02-16 16:41:29 -0300

    258 Redirecionamento

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -6,7 +6,7 @@
         $login = new Login($_POST);
         try {
             $user = $login->checkLogin();
-            echo "Usuário {$user->name} logado";
+            header("Location: day_records.php");
         }catch(AppException $e) {
             $exception = $e;
         }
--- src/views/template/messages.php
+++ src/views/template/messages.php
@@ -1,30 +1,37 @@
 <?php
+$errors = [];
 
-    $errors = [];
+if(isset($_SESSION['message'])) {
+    $message = $_SESSION['message'];
+    unset($_SESSION['message']);
+} elseif(isset($exception)) {
+    $message = [
+        'type' => 'error',
+        'message' => $exception->getMessage()
+    ];
 
-    if($exception) {
-        $message = [
-            'type' => 'error',
-            'message' => $exception->getMessage()
-        ];
-
-        if(get_class($exception) === 'ValidationException') {
-            $errors = $exception->getErrors();
-        }
+    if(get_class($exception) === 'ValidationException') {
+        $errors = $exception->getErrors();
     }
+} else {
+    $message = [
+        'type' => '',
+        'message' => ''
+    ];
+}
 
-    
-    $alertType = '';
+$alertType = '';
 
-    if($message['type'] === 'error') {
-        $alertType = 'danger';
-    } else {
-        $alertType = 'success';
-    }
+if($message['type'] === 'error') {
+    $alertType = 'danger';
+} elseif($message['type'] <> '' && $message['type'] <> 'error') {
+    $alertType = 'success';
+}
 ?>
 
-<?php if($message): ?>
-    <div role="alert" class="my-3 alert alert-<?= $alertType ?>" >
-        <?= $message['message']?>
+<?php if($message['type'] <> ''): ?>
+    <div role="alert"
+        class="my-3 alert alert-<?= $alertType ?>">
+        <?= $message['message'] ?>
     </div>
 <?php endif ?>
